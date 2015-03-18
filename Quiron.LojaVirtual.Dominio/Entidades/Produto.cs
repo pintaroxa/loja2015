908804722d684b42489d72b785cae0d90497d877
pintaroxa <pintaroxa@hotmail.com>
2015-03-18 13:41:56 -0300

    entity

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -10,9 +10,10 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
     {
         // campos da tabela produto.
 
-        public int ProdutoId { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao{ get; set; }
+        public string Categoria { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
     }
